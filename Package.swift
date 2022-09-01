d9ea0ae2b49c8af530fa052b954a20aabb622392
Cong Le <longchike@gmail.com>
2022-09-01 00:18:18 -0700

    - Initial set up

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OnboardingKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "OnboardingKit",
+            targets: ["OnboardingKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/snapkit/snapkit", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "OnboardingKit",
+            dependencies: [
+                .product(name: "SnapKit", package: "snapkit")
+            ]),
+        .testTarget(
+            name: "OnboardingKitTests",
+            dependencies: ["OnboardingKit"]),
+    ]
+)
