b2c774aa016fd6853af66aa936b063cff2688f1c
Cong Le <longchike@gmail.com>
2022-09-03 00:10:31 -0700

    - Set up transition view

--- Sources/OnboardingKit/Controllers/OnboardingViewController.swift
+++ Sources/OnboardingKit/Controllers/OnboardingViewController.swift
@@ -12,7 +12,7 @@ class OnboardingViewController: UIViewController {
     private let tintColor: UIColor
     
     private lazy var transitionView: TransitionView = {
-        let view = TransitionView()
+        let view = TransitionView(slides: slides, tintColor: tintColor)
         return view
     }()
     
